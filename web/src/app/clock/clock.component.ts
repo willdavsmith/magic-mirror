9e63a50c514921d64392d3aa4a586c80c5e7d430
willdavsmith <willdavsmith@gmail.com>
2020-07-24 01:48:19 -0400

    added clock component

--- web/src/app/clock/clock.component.ts
+++ web/src/app/clock/clock.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private timer;
+  public date: string;
+  public time: string;
+  public ampm: string;
+  public seconds: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  tick() {
+    const locale = moment(Date.now());
+    this.date = locale.format('dddd, MMMM Do, YYYY');
+    this.time = locale.format('h:mm');
+    this.ampm = locale.format('a');
+    this.seconds = locale.format('ss');
   }
 
+  ngOnInit(): void {
+    this.tick();
+    this.timer = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timer);
+  }
 }
