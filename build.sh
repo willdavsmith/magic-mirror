80fe086c70fc21807dda4cfb2935b8097a980694
willdavsmith <willdavsmith@gmail.com>
2020-08-09 04:07:25 -0400

    building and deploying for ARM architecture

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo building Angular app
 cd web
 npm i
 ng build --prod="true"
@@ -7,8 +8,9 @@ cd ..
 rm -r -f ./release
 mkdir -p ./release/web
 cp -r ./web/dist ./release/web/
+echo successfully built and copied Angular app to ./release/web/
 GOOS=linux
-GOARCH=amd64
+GOARCH=arm
 go_executable_output_file=./release/dashboard
 echo building go executable for $GOOS $GOARCH, output will be $go_executable_output_file
 env CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -a -o ./$go_executable_output_file
